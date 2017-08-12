ebf22b14ae042b7c77c6552d72c53111533b32c1
joytou <1540294142@qq.com>
2017-08-12 11:11:57 +0800

    improve

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 					  if ($update)
 					  {
 
-						  window.document.getElementById("JOYTOUupdate").innerHTML = "JOYTOU发布新版本" + $online_versionname + "了，<a href=\"http://joytou.nets.hk/log/\">查看详情</a>或者<a href=\"https://github.com/joytou/joytou.github.io/archive/1.0.1.zip\">直接下载</a>";
+						  $("#JOYTOUupdate").append("JOYTOU发布新版本" + $online_versionname + "了，<a href=\"http://joytou.nets.hk/log/\">查看详情</a>或者<a href=\"https://github.com/joytou/joytou.github.io/archive/1.0.1.zip\">直接下载</a>");
 
 					  }else{
 						  JOYTOUdismiss();
