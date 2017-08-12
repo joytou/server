b998629a7dd54bc3f5084694af5c73594dbca0a9
joytou <1540294142@qq.com>
2017-08-12 11:00:39 +0800

    improve

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
 					  var $online_versioncode = 1;
 					  var $online_versionname = "1.0.1";
-					  var $update = $versioncode < $online_versioncode;
+					  var $update = ($versioncode < $online_versioncode);
 					  if ($update)
 					  {
 
 						  window.document.getElementById("JOYTOUupdate").innerHTML = "JOYTOU发布新版本" + $online_versionname + "了，<a href=\"http://joytou.nets.hk/log/\">查看详情</a>或者<a href=\"https://github.com/joytou/joytou.github.io/archive/1.0.1.zip\">直接下载</a>";
 
+					  }else{
+						  JOYTOUdismiss();
 					  }
 				  });
 
