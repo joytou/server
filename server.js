fc2fd03ed3396c5ef7f4a3345055395c319dd1c7
joytou <1540294142@qq.com>
2017-08-16 23:14:41 +0800

    Support multiple languages

--- server.js
+++ server.js
@@ -5,7 +5,20 @@ $(document).ready(function() {
 					  if ($update)
 					  {
 
-						  $("#JOYTOUupdate").append("JOYTOU发布新版本" + $online_versionname + "了，<a href=\"http://joytou.nets.hk/log/\">查看详情</a>或者<a href=\"https://github.com/joytou/joytou.github.io/archive/1.0.1.zip\">直接下载</a>");
+						  switch($lang)
+								{
+								case "cn":
+								    $("#JOYTOUupdate").append("JOYTOU发布新版本" + $online_versionname + "了，<a href=\"http://joytou.nets.hk/2017/08/04/Log/\">查看详情</a>或者<a href=\"https://github.com/joytou/joytou.github.io/archive/1.0.1.zip\">直接下载</a>");
+								    break;
+								case "tw":
+								    $("#JOYTOUupdate").append("JOYTOU發布新版本" + $online_versionname + "了，<a href=\"http://joytou.nets.hk/2017/08/04/Log-tw/\">查看詳情</a>或者<a href=\"https://github.com/joytou/joytou.github.io/archive/1.0.1.zip\">直接下載</a>");
+								    break;
+								case "en":
+									$("#JOYTOUupdate").append("JOYTOU has released a new version " + $online_versionname + ", choose to <a href=\"http://joytou.nets.hk/2017/08/04/Log-en/\">view the details</a> or <a href=\"https://github.com/joytou/joytou.github.io/archive/1.0.1.zip\">directly download</a>");
+									break;
+								default:
+									$("#JOYTOUupdate").append("JOYTOU has released a new version " + $online_versionname + ", choose to <a href=\"http://joytou.nets.hk/2017/08/04/Log-en/\">view the details</a> or <a href=\"https://github.com/joytou/joytou.github.io/archive/1.0.1.zip\">directly download</a>");
+								}
 
 					  }else{
 						  JOYTOUdismiss();
